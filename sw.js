a4897cd2d60342040ed05db468324a84636cffb1
davidkmck <108087097+davidkmck@users.noreply.github.com>
2026-06-27 11:55:25 -0400

    Update cache name to daily-app-v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'daily-app-v3';
+const CACHE_NAME = 'daily-app-v4';
 const ASSETS = [
   '/',
   '/index.html',
@@ -7,6 +7,7 @@ const ASSETS = [
   '/apps/good-day/index.html',
   '/apps/poop-tracker/index.html',
   '/apps/24-hour-memory/index.html',
+  '/icon.png',
   '/apps/rain/index.html'
   // Add all CSS/JS files for the sub-apps here
 ];
